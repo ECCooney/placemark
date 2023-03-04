11aced6eadc0796834b7ee35bbdce00c7f298688
Ellen Cooney <elleclarec@gmail.com>
2023-03-04 14:32:07 +0000

    first commit

---
+++ src/models/json/placemark-json-store.js
@@ -0,0 +1,44 @@
+import { v4 } from "uuid";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+
+const db = new Low(new JSONFile("./src/models/json/placemarks.json"));
+db.data = { placemarks: [] };
+
+export const placemarkJsonStore = {
+  async getAllPlacemarks() {
+    await db.read();
+    return db.data.placemarks;
+  },
+
+  async addPlacemark(placemark) {
+    await db.read();
+    placemark._id = v4();
+    db.data.placemarks.push(placemark);
+    await db.write();
+    return placemark;
+  },
+
+  async getPlacemarkById(id) {
+    await db.read();
+    const list = db.data.placemarks.find((placemark) => placemark._id === id);
+    return list;
+  },
+
+  async getUserPlacemarks(userid) {
+    await db.read();
+    return db.data.placemarks.filter((placemark) => placemark.userid === userid);
+  },
+
+  async deletePlacemarkById(id) {
+    await db.read();
+    const index = db.data.placemarks.findIndex((placemark) => placemark._id === id);
+    if (index !== -1) db.data.placemarks.splice(index, 1);
+    await db.write();
+  },
+
+  async deleteAllPlacemarks() {
+    db.data.placemarks = [];
+    await db.write();
+  },
+};
